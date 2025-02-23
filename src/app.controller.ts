7a8c740d3fc6f6b673ed531b7bcd4a6fea012869
Guilherme Canoa Amador de Oliveira <guilherme.c.oliveira@accenture.com>
2025-02-23 19:21:32 -0300

    feature: rota pra baixar varios videos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,4 +14,10 @@ export class AppController {
   downloadVideo(@Query() query: any): Promise<string> {
     return this.appService.downloadVideo(query);
   }
+  
+  @Get('video/downloadMultiple')
+  downloadMultipleVideo(@Query() query: any): Promise<string> {
+
+    return this.appService.downloadMultipleVideo(query);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import ytdl from "@distube/ytdl-core";
 
-
+// TODO FAZER BAIXAR COM QUALIDADE BOA
 
 @Injectable()
 export class AppService {
@@ -10,16 +10,41 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async downloadVideo(querystring: {url: string, format?: string}): Promise<string> {
-    try {
-          
-          // Download a video
-          await ytdl(querystring.url).pipe(require("fs").createWriteStream("video.mp4"));
+  async downloadMultipleVideo(querystring: {urls: string, format?: string}): Promise<string> {
+    try {    
+      const urls = querystring.urls.split(',');
+      for (const url of urls) {
+        // Get video info
+        let videoTitle = ';'
+        await ytdl.getBasicInfo(url).then(info => {
+          console.log(info.videoDetails.title);
+          videoTitle = info.videoDetails.title.trim().replace(/[^a-zA-Z0-9]/g, "_");
+        });
 
+        // Download a video
+        await ytdl(url).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
+      }
+
+      return new Promise((resolve, reject) => {
+        return resolve('success');
+      })
+    } catch (error) {
+      throw new Error(`Failed to download video: ${error.message}`);
+    }
+  }
+
+  async downloadVideo(querystring: {url: string, quality?: string}): Promise<string> {
+    try {
+          const { quality } = querystring;
           // Get video info
+          let videoTitle = ';'
           await ytdl.getBasicInfo(querystring.url).then(info => {
             console.log(info.videoDetails.title);
+            videoTitle = info.videoDetails.title.trim().replace(/[^a-zA-Z0-9]/g, "_");
           });
+
+          // Download a video
+          await ytdl(querystring.url, { quality: quality || 'highest' }).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
 
           // Get video info with download formats
           // ytdl.getInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
