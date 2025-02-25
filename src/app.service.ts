4c17f1b392d12b677d331e3a8c37fc7482e47176
Guilherme Canoa Amador de Oliveira <guilherme.c.oliveira@accenture.com>
2025-02-25 16:21:03 -0300

    refactor: one function to download

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,19 +10,43 @@ export class AppService {
     return 'Hello World!';
   }
 
-  async downloadMultipleVideo(querystring: {urls: string, format?: string}): Promise<string> {
+  async downloadVideoYTDL(url:string, options: object): Promise<string> {
+    try {
+          // Get video info
+          let videoTitle = ';'
+          await ytdl.getBasicInfo(url).then(info => {
+            console.log(info.videoDetails.title);
+            videoTitle = info.videoDetails.title.trim().replace(/[^a-zA-Z0-9]/g, "_");
+          });
+
+          // Get video info with download formats
+          await ytdl.getInfo(url).then(info => {
+            console.log(info.formats);
+          });
+
+          // Download a video
+          await ytdl(url, options).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
+
+
+          return new Promise((resolve, reject) => {
+            return resolve('success');
+          })
+    }
+    catch (error) {
+      throw new Error(`Failed to download video: ${error.message}`);
+    }
+  }
+
+  async downloadMultipleVideo(querystring: {urls: string, quality?: string}): Promise<string> {
     try {    
       const urls = querystring.urls.split(',');
+      const { quality } = querystring;
+      const options = {
+        quality,
+      }
       for (const url of urls) {
-        // Get video info
-        let videoTitle = ';'
-        await ytdl.getBasicInfo(url).then(info => {
-          console.log(info.videoDetails.title);
-          videoTitle = info.videoDetails.title.trim().replace(/[^a-zA-Z0-9]/g, "_");
-        });
-
-        // Download a video
-        await ytdl(url).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
+        const response = await this.downloadVideoYTDL(url, options);
+        console.log(url, response);
       }
 
       return new Promise((resolve, reject) => {
@@ -35,26 +59,14 @@ export class AppService {
 
   async downloadVideo(querystring: {url: string, quality?: string}): Promise<string> {
     try {
-          const { quality } = querystring;
-          // Get video info
-          let videoTitle = ';'
-          await ytdl.getBasicInfo(querystring.url).then(info => {
-            console.log(info.videoDetails.title);
-            videoTitle = info.videoDetails.title.trim().replace(/[^a-zA-Z0-9]/g, "_");
-          });
-
-          // Get video info with download formats
-          await ytdl.getInfo(querystring.url).then(info => {
-            console.log(info.formats);
-          });
-
-          // Download a video
-          await ytdl(querystring.url).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
-
-
-          return new Promise((resolve, reject) => {
-            return resolve('success');
-          })
+      const { url, quality } = querystring;
+      const options = {
+        quality,
+      }
+      const response = await this.downloadVideoYTDL(url, options);
+      return new Promise((resolve, reject) => {
+        return resolve(response);
+      })
     } catch (error) {
       throw new Error(`Failed to download video: ${error.message}`);
     }
