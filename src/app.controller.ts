4b3ac9ced7692bbb91144aa73efd02158fb88fa1
Guilherme Canoa Amador de Oliveira <guilherme.c.oliveira@accenture.com>
2025-02-26 22:33:04 -0300

    feat: download video and audio separatly

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,11 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('video/download')
   downloadVideo(@Query() query: any): Promise<string> {
     return this.appService.downloadVideo(query);
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,14 +3,64 @@ import * as fs from 'fs';
 import ytdl from "@distube/ytdl-core";
 
 // TODO FAZER BAIXAR COM QUALIDADE BOA
+// PARAR DE QUEBRAR A APLICAÇÃO QUANDO DER ERRO
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  HDQualityLabels: string[] = [
+    '1080p',
+    '720p'
+  ]
+
+  selectVideoFormats(formats: ytdl.videoFormat[]): ytdl.videoFormat[] {
+
+    const response : ytdl.videoFormat[] = [];
+
+    // Filter video formats with audio and video
+    const formatsWithAudioAndVideo = formats.filter((format) =>
+      format.hasAudio
+      && format.hasVideo
+    )
+
+    response.push(formatsWithAudioAndVideo[0]); // pelo menos ter 1 video completo pra baixar sempre
+
+    const HDVideoWithAudioAndVideo = formatsWithAudioAndVideo.find(format =>
+      this.HDQualityLabels.includes(format.qualityLabel)
+      && format.mimeType === 'video/mp4'
+    )
+
+    if (HDVideoWithAudioAndVideo !== undefined) {
+      // Already got the HD video with audio and video, return format
+      response.push(HDVideoWithAudioAndVideo);
+      return response;
+    }
+
+    // se nao tem video em HD com audio e video, pegar um video em HD separado, depois pegar um audio em HD separado e juntar os dois
+    const HDVideo = formats.find( format =>
+      this.HDQualityLabels.includes(format.qualityLabel)
+      && format.hasVideo
+      && !format.hasAudio
+      && format.container === 'mp4'
+      && format.codecs.indexOf('avc1') !== -1 //unico formato que rodou no meu player! usar ele sempre
+    )
+    const HDAudio = formats.find( format =>
+      format.mimeType.indexOf('audio/mp4;') !== -1
+      // && format.approxDurationMs === HDVideo.approxDurationMs
+      && !format.hasVideo
+      && format.hasAudio
+    )
+
+    if (HDVideo !== undefined && HDAudio !== undefined) {
+      response.push(HDVideo);
+      response.push(HDAudio);
+      return response;
+    }
+    
+    return response;
   }
 
-  async downloadVideoYTDL(url:string, options: object): Promise<string> {
+  async downloadVideoYTDL(url:string, options: { format?: ytdl.videoFormat }): Promise<string> {
     try {
           // Get video info
           let videoTitle = ';'
@@ -20,13 +70,27 @@ export class AppService {
           });
 
           // Get video info with download formats
-          await ytdl.getInfo(url).then(info => {
-            console.log(info.formats);
-          });
+          let info = await ytdl.getInfo(url);
 
-          // Download a video
-          await ytdl(url, options).pipe(require("fs").createWriteStream(`${videoTitle}.mp4`));
+          const selectedFormats = this.selectVideoFormats(info.formats)
 
+          for (const format of selectedFormats) {
+            options.format = format;
+            console.log('Downloading video with format:', format);
+            
+            const videoTitleFile = videoTitle
+            + (format.hasVideo ? '_video' : '')
+            + (format.hasAudio ? '_audio' : '')
+            + (format.qualityLabel ? '_'+ format.qualityLabel : '');
+
+            const writeStream = fs.createWriteStream(`${videoTitleFile}.mp4`);
+            ytdl(url, options).pipe(writeStream);
+
+            await new Promise((resolve, reject) => {
+              writeStream.on('finish', resolve);
+              writeStream.on('error', reject);
+            });
+          }
 
           return new Promise((resolve, reject) => {
             return resolve('success');
@@ -37,13 +101,12 @@ export class AppService {
     }
   }
 
-  async downloadMultipleVideo(querystring: {urls: string, quality?: string}): Promise<string> {
+  async downloadMultipleVideo(querystring: { urls: string }): Promise<string> {
     try {    
       const urls = querystring.urls.split(',');
-      const { quality } = querystring;
       const options = {
-        quality,
       }
+
       for (const url of urls) {
         const response = await this.downloadVideoYTDL(url, options);
         console.log(url, response);
@@ -57,11 +120,10 @@ export class AppService {
     }
   }
 
-  async downloadVideo(querystring: {url: string, quality?: string}): Promise<string> {
+  async downloadVideo(querystring: { url: string }): Promise<string> {
     try {
-      const { url, quality } = querystring;
+      const { url } = querystring;
       const options = {
-        quality,
       }
       const response = await this.downloadVideoYTDL(url, options);
       return new Promise((resolve, reject) => {
